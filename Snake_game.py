c65f2dde6c599c1aaae034e65dd6524a7eab16a9
Esh-ankhi <ankhibhattacharya9@gmail.com>
2025-08-15 20:39:31 +0530

    Created the Snake body

--- Snake_game.py
+++ Snake_game.py
@@ -18,7 +18,7 @@ for i in pos:
     else:
         cursor.color("black")
     j=j+1
-    cursor.penup
+    cursor.penup()
     cursor.goto(i)
     segment.append(cursor)
 
@@ -27,4 +27,14 @@ game_is_on=True
 while game_is_on:
     screen.update()
     time.sleep(0.1)
-    #bsjlfi
+
+    for seg_num in range(len(segment)-1,0,-1):
+        new_x=segment[seg_num-1].xcor()
+        new_y=segment[seg_num-1].ycor()
+        segment[seg_num].goto(new_x,new_y)
+    segment[0].forward(20)
+
+   
+   
+
+screen.exitonclick()
